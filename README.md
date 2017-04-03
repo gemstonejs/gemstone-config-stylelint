75f76a4a8501ad026bfb83d93093fff7dcbf1adc
Ralf S. Engelschall <rse@engelschall.com>
2017-04-03 23:32:10 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gemstone-config-stylelint
-StyleLint Linting Configuration for CSS Files
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Config StyleLint
+=========================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-config-stylelint.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-config-stylelint.png" alt=""/>
+
+About
+-----
+
+This is the [StyleLint](https://stylelint.io/) configuration for CSS files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-config-stylelint.js
@@ -0,0 +1,34 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  exported configuration  */
+module.exports = {
+    "extends": require.resolve("stylelint-config-standard"),
+    "rules": {
+        "indentation":                                  4,
+        "color-hex-case":                               [ "lower" ],
+        "color-hex-length":                             "long",
+        "rule-nested-empty-line-before":                null,
+        "rule-empty-line-before":                       [ "always-multi-line", {
+                                                            "except": [
+                                                                "after-single-line-comment",
+                                                                "inside-block-and-after-rule",
+                                                                "first-nested"
+                                                            ],
+                                                            "ignore": [
+                                                                "after-comment",
+                                                                "inside-block"
+                                                            ]
+                                                        } ],
+        "declaration-empty-line-before":                "never",
+        "declaration-colon-space-before":               "never",
+        "declaration-colon-space-after":                null,
+        "shorthand-property-no-redundant-values":       null,
+        "selector-descendant-combinator-no-non-space":  null,
+        "block-opening-brace-space-before":             null
+    }
+}
+
